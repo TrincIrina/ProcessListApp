7a3427f50933394e4b11c51d4bc63341f337c8ad
User <User@HOME>
2024-04-01 03:24:27 +0700

    Добавьте файлы проекта.

---
+++ InformationProcess.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProcessListApp
+{
+    /// <summary>
+    /// Логика взаимодействия для InformationProcess.xaml
+    /// </summary>
+    public partial class InformationProcess : Window
+    {
+        private Process process;
+        public InformationProcess(int id)
+        {
+            InitializeComponent();
+
+            TextProcess(id);
+        }
+
+        private void TextProcess(int id)
+        {
+            process = Process.GetProcessById(id);
+            infListBox.Items.Add($"Handle: {process.Handle}");
+            infListBox.Items.Add($"PID: {process.Id}");
+            infListBox.Items.Add($"Name: {process.ProcessName}");
+            infListBox.Items.Add($"Priority: {process.BasePriority}");
+            infListBox.Items.Add($"StartTime: {process.StartTime}");
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ProcessListApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Process[] processes;        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ProcessList();           
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void informationButton_Click(object sender, RoutedEventArgs e)
+        {
+            string proc = processListBox.SelectedItem.ToString();
+            int ind = proc.IndexOf("-");
+            proc = proc.Remove(ind - 1);
+            if (proc != null)
+            {
+                InformationProcess information = new InformationProcess(Convert.ToInt32(proc));
+                information.Show();
+            }
+            else
+            {
+                MessageBox.Show(
+                    "The process is not selected",
+                    "Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            processListBox.Items.Clear();
+            ProcessList();            
+        }
+
+        private void ProcessList()
+        {            
+            processes = Process.GetProcesses();
+            processListBox.Items.Add("PID - Process name");
+            foreach (Process process in processes)
+            {
+                processListBox.Items.Add($"{process.Id} - {process.ProcessName}");
+            }
+        }
+    }
+}
